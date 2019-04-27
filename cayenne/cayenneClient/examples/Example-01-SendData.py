37e525b0085586ab47158e11d468ccf63c63bb19
Uli Raich <uli.taich@cern.ch>
2019-04-27 19:07:29 +0200

    corrected examples

--- cayenne/cayenneClient/examples/Example-01-SendData.py
+++ cayenne/cayenneClient/examples/Example-01-SendData.py
@@ -4,9 +4,9 @@ import time
 import logging
 
 # Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-MQTT_USERNAME  = "7c70a330-69af-11e8-a76a-fdebb8d0010d"
-MQTT_PASSWORD  = "32d184add41570759dd1735fa464cef7e62876a4"
-MQTT_CLIENT_ID = "d6accfc0-6541-11e9-bdb5-dfd20f02ea3f"
+MQTT_USERNAME  = "CAYENNE_USERNAME"
+MQTT_PASSWORD  = "CAYENNE_PASSWORD"
+MQTT_CLIENT_ID = "CAYENNE_CLIENT_ID"
 
 
 client = cayenne.client.CayenneMQTTClient()
