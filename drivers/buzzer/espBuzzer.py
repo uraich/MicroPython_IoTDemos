4b7f0cbecb3dd9876e8fef5246907518cc1168b0
Uli Raich <uli.taich@cern.ch>
2019-05-07 20:16:49 +0200

    added pin numbering conditionned by platform (CPU type)

--- drivers/buzzer/espBuzzer.py
+++ drivers/buzzer/espBuzzer.py
@@ -3,11 +3,26 @@ import time
 
 from rtttl import RTTTL
 import songs
+import sys
 
-PWM_PIN=14
-speaker_pin=Pin(PWM_PIN)
+
+
+print("Testing the buzzer")
+print("Program written for the workshop on IoT at the")
+print("African Internet Summit 2019")
+print("Copyright: U.Raich")
+print("Released under the Gnu Public License")
+
+if sys.platform == "esp8266":
+    print("Running on ESP8266")
+    pwmPin = 14
+else:
+    print("Running on ESP32") 
+    pwmPin = 18
+    
+
+speaker_pin=Pin(pwmPin)
 speaker = PWM(speaker_pin)
-
 def play_tone(freq, msec):
 #    print('freq = {:6.1f} msec = {:6.1f}'.format(freq, msec))
     print("Freq: %6.1f, duration: %6.1f"%(freq,msec))
