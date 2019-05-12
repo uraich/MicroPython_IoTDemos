4e5eaf7bb69aebb8951b2fe385e2aacd0d676a43
Uli Raich <uli.taich@cern.ch>
2019-05-12 11:55:19 +0200

    modified the basic led blinking programs added a program to switch the led off added a program to change intensity through pwm

--- drivers/LED/ledOff.py
+++ drivers/LED/ledOff.py
@@ -1,5 +1,4 @@
-# helloLed.py: This is the embedded systems "Hello World! program",
-# the blinking LED: It uses the built-in LED on the CPU card
+# ledOff.py: Switches the built-in LED off
 # Both, ESP8266 and ESP32 use GPIO 2 to control the LED
 # copyright: U. Raich, 12.5.2019
 # This program was written as a demo for the AIS conference 2019, Kampala, Uganda
@@ -8,7 +7,7 @@
 from machine import Pin
 import time
 
-print("The embedded systems \"Hello World\!\" program")
+print("Switches the built-in LED off")
 print("Program written for the workshop on IoT at the")
 print("African Internet Summit 2019")
 print("Copyright: U.Raich")
@@ -16,10 +15,7 @@ print("Released under the Gnu Public License")
   
 led = Pin(2,Pin.OUT)
 
-while True:
-  led.on()
-  time.sleep(0.5)
-  led.off()
-  time.sleep(0.5)
+led.off()
+
 
 
