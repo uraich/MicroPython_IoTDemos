86a09f8aa2c7cc83c15abbfa4eea9e881cff4255
Uli Raich <uli.taich@cern.ch>
2019-05-13 20:07:36 +0200

    add graphics to oled display demo

--- drivers/oled/ssd1306Text.py
+++ drivers/oled/ssd1306Text.py
@@ -2,7 +2,6 @@
 
 import machine, time, ssd1306
 import sys
-from math import sin,cos,pi
 
 print("Testing the ssd1306 OLED display")
 print("Program written for the workshop on IoT at the")
@@ -40,66 +39,12 @@ else:
     if device == addrOled:
       oledIsConnected = True
       
-def clear():
-    oled.fill(0)
-
-def testDrawLine():
-    for i in range(0,wSize,4):
-        oled.line(0,0,i,hSize-1,1)
-        oled.show()
-        time.sleep(0.1)
-    for i in range(0,hSize,4):
-        oled.line(0,0,wSize-1,hSize-i-4,1)
-        oled.show()
-        time.sleep(0.1)
-        
-def drawCircle(orig_x,orig_y,radius):
-    for i in range(0,round(50*pi)+1):
-        x = round(radius * cos(i/25)) + orig_x + wSize//2
-        y = round(radius * sin(i/25)) + orig_y + hSize //2
-        if x < wSize and y < hSize:
-            oled.pixel(x,y,1)
-        oled.show()
-        
-def testDrawRect():
-    for i in range (0,hSize//2,2):
-        oled.rect(i,i,wSize-2*i,hSize-2*i,1)
-        oled.show()
-        time.sleep(0.1)
-
-def testFillRect():
-    for i in range(0,hSize//2,3):
-        oled.fill_rect(i,i,i*2,i*2,1)
-        oled.show()
-        time.sleep(0.1)
- 
-def drawSin():
-    oled.hline(0,hSize//2,wSize,1)
-    oled.vline(1,0,hSize,1)
-    oled.show()
-    for i in range(0,wSize):
-        oled.pixel(i,round(-sin(2*pi*i/wSize)*hSize//2)+hSize//2,1)
-        oled.show()
-    oled.text("sin(x)",wSize//4,hSize//5,1)
-    oled.show()
-     
 if oledIsConnected:
   oled = ssd1306.SSD1306_I2C(wSize, hSize, i2c, addrOled)
   oled.fill(0)
-  testDrawLine()
-  clear()
-  testDrawRect()
-  clear()
-  testFillRect()
-  clear()
-  drawCircle(0,0,20)
-  drawCircle(15,5,10)
-  drawCircle(-10,-15,15)
-  drawCircle(5,15,5)
-  time.sleep(1)
-  clear()
-  drawSin()
-   
+  oled.text("192.168.",0,0)
+  oled.text("1.150",0,10)
+  oled.show()
 else:
   print('! No i2c display')
 time.sleep_ms(5000)
