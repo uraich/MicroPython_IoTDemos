92a3ef4cfeaeed97917a2e1560d0815c1958337b
Uli Raich <uli.taich@cern.ch>
2019-06-11 11:02:44 +0200

    added function example

--- session1/while.py
+++ session1/while.py
@@ -10,3 +10,14 @@ while (counter < 5):
 print("\nfor loop")
 for i in range(0,5):
     print(i)
+    
+# The Fibonacci series
+a,b=0,1
+print("The Fibonacci series")
+while b<200:
+    print(b)
+    a,b = b, a+b
+    
+words = ['Tunis','Gaborone','Nairobo','Dakar','Kampala']
+for w in words:
+    print(w,'has',len(w),'characters')
